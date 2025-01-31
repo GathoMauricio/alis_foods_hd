15f151639430cce2f584a52b435ff43b09ac273a
OMJS2769 <mauricio2769@outlook.es>
2025-01-31 14:06:08 -0600

    suspencion estado de cuenta

---
+++ resources/views/layouts/estatus_pago.blade.php
@@ -0,0 +1,16 @@
+<!-- Modal -->
+<div class="modal fade" id="modal_estatus_pago" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Servicio Suspendido</h5>
+            </div>
+            <div class="modal-body">
+                El servicio se encuentra temporalmente suspendio.
+                <br>
+                Favor de revisar el estado de su facturación ya que su estado de cuenta presenta un adeudo.
+            </div>
+        </div>
+    </div>
+</div>
